a3587242f2ac82c64899f8914fb910030a6bbec4
DarkThomaslol <92427564+DarkThomaslol@users.noreply.github.com>
2023-05-08 12:16:42 +0200

    enemy script

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -11,12 +11,17 @@ public class EnemyScript : MonoBehaviour
     };
     public types type;
 
-    public SpriteRenderer body;
+    public GameObject body;
     [SerializeField] private CircleCollider2D triggerCol;
-
+    [SerializeField] private float speed = 10;
+ 
     private GameObject target;
-    Vector3 lookPos;
-    [SerializeField] private int smoothTurn = 2;
+    bool canAim = false;
+
+    public GameObject bullet;
+    private float shootingTime;
+    [SerializeField] private float fireRate = 3f;
+    [SerializeField] private float shootingPower = 20f;
 
     void Start()
     {
@@ -27,31 +32,85 @@ public class EnemyScript : MonoBehaviour
     {
         if(type == types.melee)
         {
-            body.color = new Color(1, 0, 0);
+            body.GetComponent<SpriteRenderer>().color = new Color(1, 0, 0);
             triggerCol.radius = 4;
-            Aim();
+
+            if (canAim)
+            {
+                float range = Vector2.Distance(transform.position, target.transform.position);
+                if (range > 0)
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+                }
+
+                if (range < 1)
+                {
+                    Debug.Log("ballsM");
+                }
+            }
         }
 
         if(type == types.ranged)
         {
-            body.color = new Color(0, 0, 1);
+            body.GetComponent<SpriteRenderer>().color = new Color(0, 0, 1);
             triggerCol.radius = 8;
+
+            if (canAim)
+            {
+                float range = Vector2.Distance(transform.position, target.transform.position);
+                if (range > 4)
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+                }
+
+                if (range < 5)
+                {
+                    Fire(target.transform);
+                }
+            }
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D col)
+    private void OnTriggerStay2D(Collider2D col)
     {
-        if (col.gameObject.CompareTag("Tower"))
+        if (col.gameObject.CompareTag("Player"))
         {
             target = col.gameObject;
+            canAim = true;
+        }
+        else if (col.gameObject.CompareTag("Tower"))
+        {
+            target = col.gameObject;
+            canAim = true;
         }
     }
 
-    private void Aim()
+    private void OnTriggerExit2D(Collider2D other)
     {
-        lookPos = target.transform.position - transform.position;
-        lookPos.y = 0;
-        var rotation = Quaternion.LookRotation(lookPos);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * smoothTurn);
+        if (target)
+        {
+            canAim = false;
+        }
+    }
+
+    public void Idle(Vector3 pos)
+    {
+        float range = Vector2.Distance(transform.position, pos);
+        if (range > 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, pos, speed * Time.deltaTime);
+        }
+    }
+
+    private void Fire(Transform target)
+    {
+        if (Time.time > shootingTime)
+        {
+            shootingTime = Time.time + fireRate / 1;
+            Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
+            GameObject projectile = Instantiate(bullet, myPos, Quaternion.identity);
+            Vector2 direction = myPos - (Vector2)target.position;
+            projectile.GetComponent<Rigidbody2D>().velocity = -direction * shootingPower;
+        }
     }
 }
