ea79790426472fcb11053aaa02bd8765fe320c7a
DarkThomaslol <92427564+DarkThomaslol@users.noreply.github.com>
2023-05-25 16:20:15 +0200

    enemy work now i guess

--- Assets/Scripts/Enemy/MageEnemyAI.cs
+++ Assets/Scripts/Enemy/MageEnemyAI.cs
@@ -7,9 +7,9 @@ using static UnityEngine.RuleTile.TilingRuleOutput;
 [CreateAssetMenu(menuName = "EnemyAI/Mage")]
 public class MageEnemyAI : EnemyAISO
 {
-    public override void Aim(EnemyScript self, GameObject target, float closeRange, float attackRange, float farRange)
+    public override void Aim(EnemyScript self, EnemyStats stats, GameObject target, float closeRange, float attackRange, float farRange)
     {
-        base.Aim(self, target, attackRange);
+        base.Aim(self, stats, target, attackRange);
         if (Range > farRange)
         {
             self.gameObject.transform.position = Vector2.MoveTowards(self.gameObject.transform.position, target.transform.position, self.speed * Time.deltaTime);
@@ -18,9 +18,9 @@ public class MageEnemyAI : EnemyAISO
         {
             self.gameObject.transform.position = Vector2.MoveTowards(self.gameObject.transform.position, self.gameObject.transform.position - target.transform.position, self.speed * Time.deltaTime);
         }
-        else if (Range < attackRange)
+        if (Range < attackRange)
         {
-            Fire(self.body, target.transform.position, self.bullet);
+            Fire(stats, self.body, target.transform.position, self.bullet);
         }
     }
 }
--- Assets/Scripts/Enemy/SuicideEnemyAI.cs
+++ Assets/Scripts/Enemy/SuicideEnemyAI.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "EnemyAI/Suicide")]
 public class SuicideEnemyAI : EnemyAISO
 {
-    public override void Aim(EnemyScript self, GameObject target, float attackRange)
+    public override void Aim(EnemyScript self, EnemyStats stats, GameObject target, float attackRange)
     {
-        base.Aim(self, target, attackRange);
+        base.Aim(self, stats, target, attackRange);
         if (Range > attackRange)
         {
             self.gameObject.transform.position = Vector2.MoveTowards(self.gameObject.transform.position, target.transform.position, self.speed * Time.deltaTime);
