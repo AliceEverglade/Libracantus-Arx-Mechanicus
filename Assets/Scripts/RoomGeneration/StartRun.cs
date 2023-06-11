1d30c92c02b06ed2292e9c906f1b9e20118e694d
Alice Everglade <AliceEverglade@gmail.com>
2023-06-11 18:54:10 +0200

    room gen data push

--- Assets/Scripts/RoomGeneration/StartRun.cs
+++ Assets/Scripts/RoomGeneration/StartRun.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StartRun : MonoBehaviour
@@ -14,6 +15,7 @@ public class StartRun : MonoBehaviour
     [SerializeField] private float waitTime = 4;
     [SerializeField] private bool exitRoomSpawned;
     [SerializeField] private bool doneChecking = false;
+    private float roomSize = 16;
 
     [Header("RoomList")]
     [SerializeField] private List<GameObject> roomList;
@@ -36,25 +38,40 @@ public class StartRun : MonoBehaviour
 
     private void OnDisable()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         RoomsSpawned = library.RoomCount;
-        if(waitTime < 0 && !doneChecking)
+        if (waitTime < 0 && !doneChecking)
         {
             GameObject exit = null;
-            GameObject replacedExitRoom = null;
-            foreach(GameObject room in roomList)
+            GameObject replacedRoom = null;
+            GameObject newRoom = null;
+            foreach (GameObject room in roomList)
             {
-                //Debug.Log($"checking room: {room.name}");
                 List<RoomData.OpeningDirections> dirs = room.GetComponent<RoomHolder>().roomData.RoomOpenings;
+                List<RoomData.OpeningDirections> replacementDirs = new List<RoomData.OpeningDirections>();
+                foreach (RoomData.OpeningDirections dir in dirs)
+                {
+                    if (CheckSurroundings(dir, room.transform.position, out RoomData.OpeningDirections newRoomDir))
+                    {
+                        replacementDirs.Add(newRoomDir);
+                    }
+                }
+                /*
+                if(replacementDirs != null)
+                {
+                    Debug.Log($"replacing room {}");
+                    RoomData newRoomData = library.GetRoom(replacementDirs);
+                    newRoom = ReplaceRoom(room, newRoomData);
+                }*/
                 if (!exitRoomSpawned && dirs != null)
                 {
                     int counter = 0;
-                    
+
                     foreach (RoomData.OpeningDirections dir in dirs)
                     {
                         if (library.Exit.RoomOpenings.Contains(dir))
@@ -66,27 +83,32 @@ public class StartRun : MonoBehaviour
                     {
                         Debug.Log($"{room.name} is replaced with the exit");
                         exit = ReplaceRoom(room, library.Exit);
-                        
+
                         exitRoomSpawned = true;
                     }
                 }
-
-                
-                //check if rooms need to be replaced by other rooms based on their surrounding rooms
-                //check if there are rooms to each opening, if not then put a wall or corresponding closing room there
             }
-            if(exit != null)
+            if (exit != null)
             {
-                roomList.Remove(replacedExitRoom);
+                roomList.Remove(replacedRoom);
                 roomList.Add(exit);
+                foreach (RoomData.OpeningDirections dir in library.Exit.RoomOpenings)
+                {
+                    CheckSurroundings(dir, exit.transform.position, out RoomData.OpeningDirections exitRoomDir);
+                }
             }
             else
             {
                 Debug.Log("Exit Room not found");
             }
+            if (newRoom != null)
+            {
+                roomList.Remove(replacedRoom);
+                roomList.Add(newRoom);
+            }
             doneChecking = true;
         }
-        else if(!doneChecking)
+        else if (!doneChecking)
         {
             waitTime -= Time.deltaTime;
         }
@@ -97,12 +119,67 @@ public class StartRun : MonoBehaviour
     {
         Vector3 pos = original.transform.position;
         Destroy(original);
-        GameObject replaced = Instantiate(replacer.Prefab,pos, Quaternion.identity);
+        GameObject replaced = Instantiate(replacer.Prefab, pos, Quaternion.identity);
         return replaced;
     }
 
     private void AddRoomToList(GameObject room)
     {
         roomList.Add(room);
+    }
+
+    private bool CheckSurroundings(RoomData.OpeningDirections direction, Vector3 origin, out RoomData.OpeningDirections newRoomDir)
+    {
+        Vector3 dir = new Vector3(0, 0, 0);
+        RoomData.OpeningDirections otherRoomDir;
+        switch (direction)
+        {
+            case RoomData.OpeningDirections.North:
+                dir = new Vector3(0, roomSize, 0);
+                otherRoomDir = RoomData.OpeningDirections.South;
+                break;
+            case RoomData.OpeningDirections.East:
+                dir = new Vector3(roomSize, 0, 0);
+                otherRoomDir = RoomData.OpeningDirections.West;
+                break;
+            case RoomData.OpeningDirections.South:
+                dir = new Vector3(0, -roomSize, 0);
+                otherRoomDir = RoomData.OpeningDirections.North;
+                break;
+            case RoomData.OpeningDirections.West:
+                dir = new Vector3(-roomSize, 0, 0);
+                otherRoomDir = RoomData.OpeningDirections.East;
+                break;
+            default:
+                newRoomDir = RoomData.OpeningDirections.North;
+                return false;
+        }
+        List<GameObject> targets = new List<GameObject>();
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(new Vector2(dir.x + origin.x, dir.y + origin.y), 1);
+        foreach (Collider2D col in colliders)
+        {
+            if (col.CompareTag("Room"))
+            {
+                targets.Add(col.gameObject);
+            }
+        }
+        if (targets.Count > 0)
+        {
+            foreach (GameObject target in targets)
+            {
+                if (target.GetComponent<RoomHolder>().roomData.RoomOpenings.Contains(otherRoomDir))
+                {
+                    newRoomDir = direction;
+                    return true;
+                }
+            }
+        }
+        else
+        {
+            Instantiate(library.Wall.Prefab, new Vector3(origin.x + dir.x, origin.y + dir.y, 0), Quaternion.identity);
+            newRoomDir = RoomData.OpeningDirections.North;
+        }
+        newRoomDir = RoomData.OpeningDirections.North;
+        return false;
     }
 }
