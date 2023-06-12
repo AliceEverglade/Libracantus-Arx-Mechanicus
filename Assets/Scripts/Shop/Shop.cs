813815156da2e80db9c1a07c81320d89bd39b927
Christian Oosterveen <92017111+Dutch31@users.noreply.github.com>
2023-06-12 17:29:34 +0200

    Shop Interactivity & Colliders Start

---
+++ Assets/Scripts/Shop/Shop.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] private GameObject ShopScreenOpen;
+    [SerializeField] private GameObject ShopGameObject;
+    [SerializeField] private GameObject Player;
+    [SerializeField] public BoxCollider2D _SBC2D;
+    
+    
+    [SerializeField] public PlayerMove _PM1;
+    public static bool GameIsPaused = false;
+
+    public bool ShopOpen = false;
+
+    public void Start()
+    {
+        ShopScreenOpen.SetActive(false);
+        _PM1 = Player.GetComponent<PlayerMove>();
+        _SBC2D = ShopGameObject.GetComponent<BoxCollider2D>();
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Shop Open");
+            ShopOpen = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Shop Close");
+            ShopOpen = false;
+            _PM1.enabled = true;
+            _SBC2D.enabled = true;
+
+            
+        }
+    }
+
+    public void Update()
+    {
+        if (ShopOpen == true)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                ShopScreenOpen.SetActive(true);
+                Debug.Log("Shop Has Been Accesed");
+                _PM1.enabled = false;
+                Time.timeScale = 0f;
+                GameIsPaused = true;
+                
+                _SBC2D.enabled = false;
+                // make it like the pause menu, where it stops time when you press the button and brings up the menu
+            }
+        }   
+    }
+
+    public void Resume ()
+    {
+        ShopScreenOpen.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        _PM1.enabled = true;
+        _SBC2D.enabled = true;
+    }
+}
